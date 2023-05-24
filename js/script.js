a3562d5be3050700b7f36914d9428d140557fb71
VictorNunesVN <58942853+VictorNunesVN@users.noreply.github.com>
2023-05-24 17:09:19 -0300

    criando arquivo
    Fazendo modificações no html e criando um arquivo javascript para fazer as validações

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const camposDoFormulario = document.querySelectorAll('[required]')
+camposDoFormulario.forEach((campo) => {
+    campo.addEventListener("blur", ()=> verificaCampo(campo))
+})
+
+function verificaCampo(campo){
+    
+}
