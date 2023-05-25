eefd187a3577aa330de9f77b380b70833359e8b6
VictorNunesVN <58942853+VictorNunesVN@users.noreply.github.com>
2023-05-25 08:01:15 -0300

    Update valida-cpf.js
    Correção no código: atribuição no lugar de igualdade em soma .

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,72 +1,9 @@
-// export default function ehUmCPF(campo) {
-//     const cpf = campo.value.replace(/\.|-/g, "")
-//     if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
-//         console.log("Este CPF não existe")
-//     } else {
-//         console.log("Este CPF existe")
-//     }
-// }
-
-// function validaNumerosRepetidos(cpf) {
-//     const numerosRepetidos = [
-//         '00000000000',
-//         '11111111111',
-//         '22222222222',
-//         '33333333333',
-//         '44444444444',
-//         '55555555555',
-//         '66666666666',
-//         '77777777777',
-//         '88888888888',
-//         '99999999999'
-//     ]
-
-//     return numerosRepetidos.includes(cpf)
-// }
-
-// function validaPrimeiroDigito(cpf) {
-//     let soma = 0
-//     let multiplicador = 10
-
-//     for (let tamanho = 0; tamanho < 9; tamanho++) {
-//         soma += cpf[tamanho] * multiplicador
-//         multiplicador--
-//     }
-
-//     soma = (soma * 10) % 11
-
-//     if (soma = 10 || soma == 11) {
-//         soma = 0
-//     }
-
-//     return soma != cpf[9]
-// }
-
-// function validaSegundoDigito(cpf) {
-//     let soma = 0;
-//     let multiplicador = 11;
-
-//     for (let tamanho = 0; tamanho < 10; tamanho++) {
-//         soma += cpf[tamanho] * multiplicador;
-//         multiplicador--
-//     }
-
-//     soma = (soma * 10) % 11;
-
-//     if (soma == 10 || soma == 11) {
-//         soma = 0;
-//     }
-
-//     return soma != cpf[10];
-// }
-
-
 export default function ehUmCPF(campo) {
-    const cpf = campo.value.replace(/\.|-/g, "");
+    const cpf = campo.value.replace(/\.|-/g, "")
     if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
-        console.log("Esse cpf não existe")
+        console.log("Este CPF não existe")
     } else {
-        console.log("Esse cpf existe")
+        console.log("Este CPF existe")
     }
 }
 
@@ -88,21 +25,21 @@ function validaNumerosRepetidos(cpf) {
 }
 
 function validaPrimeiroDigito(cpf) {
-    let soma = 0;
-    let multiplicador = 10;
+    let soma = 0
+    let multiplicador = 10
 
     for (let tamanho = 0; tamanho < 9; tamanho++) {
-        soma += cpf[tamanho] * multiplicador;
+        soma += cpf[tamanho] * multiplicador
         multiplicador--
     }
 
-    soma = (soma * 10) % 11;
+    soma = (soma * 10) % 11
 
     if (soma == 10 || soma == 11) {
-        soma = 0;
+        soma = 0
     }
 
-    return soma != cpf[9];
+    return soma != cpf[9]
 }
 
 function validaSegundoDigito(cpf) {
@@ -122,3 +59,4 @@ function validaSegundoDigito(cpf) {
 
     return soma != cpf[10];
 }
+
